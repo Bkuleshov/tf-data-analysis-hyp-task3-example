b259c8299fa3b15d208ac99465b6bef5466a28b5
Bogdan Kuleshov <93465270+Bkuleshov@users.noreply.github.com>
2024-05-25 20:46:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from scipy import stats
 
 chat_id = 818742406 # Ваш chat ID, не меняйте название переменной
 
@@ -8,4 +8,4 @@ def solution(x: np.array) -> bool: # Одна или две выборке на 
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return np.mean(x) > 500 * 1.06 # Ваш ответ, True или False
+    return stats.ttest_1samp(x, 500, alternative='greater').pvalue < 0.06 # Ваш ответ, True или False
